1e23f18fc8254c419b8c6e84d220e8d849371428
turbodoubiez <romberg118+github_alt@gmail.com>
2023-02-01 13:15:44 +0100

    Integrate electron and vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container container-fluid" data-bs-theme="dark">
+        <RecordedRaceList msg="This is gonna be great!" />
+    </div>
+</template>
+
+<script>
+import RecordedRaceList from './components/RecordedRaceList.vue';
+
+export default {
+    name: 'App',
+    components: {
+        RecordedRaceList
+    }
+};
+</script>
+
+<style lang="scss">
+.container {
+    --bs-gutter-y: 1.5rem;
+    padding-top: calc(var(--bs-gutter-y) * 0.5);
+    padding-bottom: calc(var(--bs-gutter-y) * 0.5);
+}
+</style>
